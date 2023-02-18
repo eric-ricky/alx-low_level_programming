56e21b113b7f68a56b039d1aabc7709441033038
Erick Ricky <ericricky200@gmail.com>
2023-02-17 21:44:24 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: prints if a number is positive negative or zero
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	for (i = 0; i < 6; i++)
+	{
+		putchar(i + 'a');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
