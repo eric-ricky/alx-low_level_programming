75f059dc3b5353190c2fdd8eab9620d652c144a0
Erick Ricky <ericricky200@gmail.com>
2023-02-17 21:18:23 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: prints if a number is positive negative or zero
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
