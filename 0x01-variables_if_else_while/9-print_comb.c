16b13826e084f13d7b1a7428d6d57ae4b0e32994
Erick Ricky <ericricky200@gmail.com>
2023-02-17 22:21:27 -0800

    task 9 04

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,14 +14,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + '0');
-		if (i != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
+
+	putchar('\n');
 	return (0);
 }
