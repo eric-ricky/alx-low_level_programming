1735831a2e87fa3d241e6d6a41610b626f7472c1
Erick Ricky <ericricky200@gmail.com>
2023-02-17 21:05:58 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: prints if a number is positive negative or zero
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
