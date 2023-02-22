c70545f1aa5958059050603a669b457a001b21fa
Erick Ricky <ericricky200@gmail.com>
2023-02-22 12:24:15 -0800

    extra task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - the entry point
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += 1;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
