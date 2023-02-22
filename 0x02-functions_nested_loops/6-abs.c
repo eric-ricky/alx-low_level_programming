d8622005a0afc184abfa5fe68e6e0e0ad4877fbb
Erick Ricky <ericricky200@gmail.com>
2023-02-22 11:07:03 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _abs - the entry point
+  * @n: integer to be computed
+  * Return: abs of n
+  *
+  */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+
+	return (n);
+}
