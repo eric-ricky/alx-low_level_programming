4c7511668929213b414c0ea95c8ff893ccd1462d
Erick Ricky <ericricky200@gmail.com>
2023-02-22 11:44:16 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - the entry point
+  * @num1: first number to be added
+  * @num2: second number to be added
+  * Return: the sumation of num1 and num2
+  */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
