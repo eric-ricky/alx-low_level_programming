babde2a63d3be1b0645ca87714f2d3711deb3823
Erick Ricky <ericricky200@gmail.com>
2023-02-21 17:23:36 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: prints if a number is positive negative or zero
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
