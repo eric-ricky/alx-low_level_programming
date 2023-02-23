eb4796f72edfa7fca75f495c7a252f4b658c0864
Erick Ricky <ericricky200@gmail.com>
2023-02-23 12:13:55 -0800

    0x04-more_funct..: task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Author: Erick Otieno
+ *
+ * Desc: Header file containing the prototypes of all functions used in the
+ *	0x04-more_functions_nested_loops
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
