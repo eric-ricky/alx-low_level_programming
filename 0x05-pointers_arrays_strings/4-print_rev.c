cc20e90fe34eabe176b616e4210eafc0bca8fa8e
Erick Ricky <ericricky200@gmail.com>
2023-02-28 10:42:08 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		putchar(*(s + i));
+	}
+
+	putchar('\n');
+}
