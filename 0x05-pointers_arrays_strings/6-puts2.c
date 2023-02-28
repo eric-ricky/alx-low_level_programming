70a41756827768c35e7eb350142049ada8f63f01
Erick Ricky <ericricky200@gmail.com>
2023-02-28 11:57:05 -0800

    task 6 01

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,14 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i) != '\0')
+	while (str[i] != '\0')
 	{
-		printf("%c", *(str + i));
-		i += 2;
+		if (i % 2 == 0)
+		{
+			printf("%c", str[i]);
+		}
+
+		i++;
 	}
 
 	printf("\n");
