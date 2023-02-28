33e3d375ded348a36c8a5cc01864bdb08caa67bb
Erick Ricky <ericricky200@gmail.com>
2023-02-28 13:05:37 -0800

    task 100 03

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,24 +10,37 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	int num = 0;
+	int res = 0;
 	int i = 0;
 
-	while (s[i] != '0')
+	while (s[i])
 	{
 		if (s[i] == '-')
 		{
 			sign *= -1;
-		} else if (s[i] >= '0' && s[i] <= '9')
+		}
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			num = num * 10 + (s[i] - '0');
-		} else if (num > 0)
+			if (res > INT_MAX / 10 || (res == INT_MAX / 10 &&
+						s[i] - '0' > INT_MAX % 10))
+			{
+				return (sign == 1 ? INT_MAX : INT_MIN);
+			}
+
+			res = res * 10 + (s[i] - '0');
+		}
+
+		if (s[i] == '+')
+		{
+			sign *= 1;
+		}
+
+		if (res != 0 && (s[i] < '0' || s[i] > '9'))
 		{
 			break;
 		}
 
 		i++;
 	}
-
-	return (sign * num);
+	return (sign * res);
 }
