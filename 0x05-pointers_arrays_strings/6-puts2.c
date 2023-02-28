cbb972ab366f896ac42a04c14f2bca63805de4bb
Erick Ricky <ericricky200@gmail.com>
2023-02-28 10:52:52 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in a string
+ * @str: the string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		printf("%c", *(str + i));
+		i += 2;
+	}
+
+	printf("\n");
+}
