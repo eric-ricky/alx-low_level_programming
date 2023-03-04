07653a78d2042bb9dec30b2ab8e9415f274e698e
Erick Ricky <ericricky200@gmail.com>
2023-03-03 18:06:54 -0800

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - entry point
+ * @s1: first string
+ * @s2: second string
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] < s2[i])
+		{
+			return (-1);
+		} else if (s1[i] > s2[i])
+		{
+			return (1);
+		}
+		i++;
+	}
+
+	return (0);
+}
