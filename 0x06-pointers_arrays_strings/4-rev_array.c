37573373f33a526a1cda8768ddf6126dfea7a33f
Erick Ricky <ericricky200@gmail.com>
2023-03-03 18:12:21 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - entry point
+ * @a: the array
+ * @n: number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
+}
